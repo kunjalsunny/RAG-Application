1f153ecda5e0633ef24268867cc212f4adf244ed
kunjalsunny <kunjalmahant72@gmail.com>
2026-02-22 23:26:01 -0500

    Chunking documents logic

--- ingestion_pipline.py
+++ ingestion_pipline.py
@@ -24,7 +24,7 @@ def load_documents(docs_path="docs"):
     print(f"Loaded {len(docs)} document pages")
 
     for i, doc in enumerate(docs[:2]):
-        print(f"\n Document: {i}:")
+        print(f"\n Document: {i+1}:")
         print(f"Source: {doc.metadata['source']}")
         print(f"Content Length: {len(doc.page_content)} characters")
         print(f"Content Preview:{doc.page_content[:30]}...")
@@ -33,11 +33,50 @@ def load_documents(docs_path="docs"):
     return docs
 
     # 2. Chunking the files
-    def split_documents(documents, chunk_size=1000, chunk_overlap=0):
-        pass
+def split_documents(documents, chunk_size=900, chunk_overlap=0):
+    print(f"Splitting the documents into chunks...")
 
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap = chunk_overlap
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    if chunks:
+        for i, chunk in enumerate(chunks[:5]):
+            print(f"\n ---Chunk{i+1}---")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)} characters")
+            print(f"Content: {chunk.page_content}")
+            print(f"------------------")
+    
+    return chunks
 
     # 3. Embedding and Storing in VectorDB
+def create_vector_store(chunks, persist_directory="db/chromadb"):
+    print(f"Create embedding and storing in chromaDB")
+
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+    print("---Create Vector store---")
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory,
+        collection_metadata={"hnsw:space":"cosine"}
+    )
+    print("---Finished create vector store---")
+    print(f"Vector store in directory: {persist_directory}")
+
+    return vectorstore
+
 
 if __name__ == "__main__":
+    #1
     documents = load_documents(docs_path='docs')
+    
+    #2
+    chunks = split_documents(documents)
+
+    #3
